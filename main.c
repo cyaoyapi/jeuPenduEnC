591c3934c778f7b3ed902d71740d45eb31a7d076
Cyrille YAO <cyaoyapi@gmail.com>
2018-07-29 03:33:16 +0200

    Test of function selectSecretWord() : OK

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 /*
+========================================================================
     The goal of the hangman game is to find a hidden word
     in less than 10 tries.
+=========================================================================
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include "pendu.h"
 
 int main()
 {
+    /* Randomly choosing of the secret word from a list of word */
+    char secret_word[LINE_SIZE] = "";
+    selectSecretWord(secret_word); // using a function defined in pendu.c
+    printf("%s",secret_word);
     return 0;
 }
