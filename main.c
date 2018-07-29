891a9ff11481693380adc0fdef681cc23c6be8cb
Cyrille YAO <cyaoyapi@gmail.com>
2018-07-29 04:31:17 +0200

    Adding a new chars' pointer for displaying the secret word.

--- main.c
+++ main.c
@@ -17,6 +17,15 @@ int main()
     /* Randomly choosing of the secret word from a list of word */
     char secret_word[LINE_SIZE] = "";
     selectSecretWord(secret_word); // using a function defined in pendu.c
+
+    /* For displaying secret word */
+    char secret_word_for_displaying[LINE_SIZE] = "";
+    for(int i=0; i < strlen(secret_word) - 1; i++){ // initialisation
+        secret_word_for_displaying[i] = '*';
+    }
+
     printf("%s",secret_word);
+    printf("%s",secret_word_for_displaying);
+
     return 0;
 }
