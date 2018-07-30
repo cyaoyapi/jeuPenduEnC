3609e66f4d9e129093b01702f2d0e4a0f2e03c10
Cyrille YAO <cyaoyapi@gmail.com>
2018-07-30 16:21:05 +0200

    Adding useful functions : cleanBuffer(); readStdin(); readLong(); readDouble()

--- utils.c
+++ utils.c
@@ -4,9 +4,83 @@
 
 #include "utils.h"
 
+
+
+/*
+    Function to to clean the buffer.
+*/
+
+void cleanBuffer(){
+    char character = 0;
+    while(character != '\n' && character != EOF){
+        character = getchar();
+    }
+}
+
+
+/*
+    Function to read a character.
+*/
+
 char readCharacter(){
     char character = 0;
     character = toupper(getchar());
     while(getchar() != '\n');
     return character;
 }
+
+
+
+/*
+    Function to read the standard in with security.
+*/
+int readStdin(char* characters, int size){
+    char* entryPosition = NULL; // To store the position of '\n' stored by function fgets
+
+    if(fgets(characters, size, stdin) != NULL){
+        entryPosition = strchr(characters,'\n');
+        if(entryPosition != NULL){
+            *entryPosition = '\0';
+
+        }else{ // If we don't get '\n' : the entry overflow the available size
+            cleanBuffer();
+        }
+        return 1;
+    }
+    else{
+        cleanBuffer();
+        return 0;
+    }
+}
+
+
+/*
+    Function to read a long entered by the user.
+*/
+
+long readLong(char* characters, int size){
+    if(readStdin(characters, size)){
+        return strtol(characters, NULL, 10);
+    }else{
+        return 0;
+    }
+}
+
+
+/*
+    Function to read a double entered by the user.
+*/
+
+double readDouble(char* characters, int size){
+    char* entryPosition = NULL; // To store and remplace ',' by '.'
+    if(readStdin(characters, size)){
+        entryPosition = strchr(characters,',');
+        if(entryPosition != NULL){
+            *entryPosition = '.';
+        }
+        return strtod(characters, NULL);
+    }else{
+        return 0;
+    }
+}
+
--- utils.h
+++ utils.h
@@ -1,7 +1,11 @@
 #ifndef UTILS_H_INCLUDED
 #define UTILS_H_INCLUDED
 
+void cleanBuffer();
 char readCharacter();
+int readStdin(char*, int);
+long readLong(char*, int);
+double readDouble(char*, int);
 
 
 #endif // UTILS_H_INCLUDED
